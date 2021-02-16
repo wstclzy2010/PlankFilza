d08c030058db1b2024a79849866396998cb2f68c
糖醋丶炒排骨 <42905727+wstclzy2010@users.noreply.github.com>
2021-02-16 13:14:48 +0800

    弹窗汉化、增加绕过盗版检测

--- PlankFilza/main.m
+++ PlankFilza/main.m
@@ -12,6 +12,7 @@
 #include "BypassAntiDebugging.h"
 #include "cicuta_virosa.h"
 #include "rootless.h"
+#include <objc/runtime.h>
 
 @implementation PatchEntry
 
@@ -22,7 +23,6 @@
     #if TESTS_BYPASS
     test_aniti_debugger();
     #endif
-    [self showAlert];
 }
 
 void error_popup(NSString *messgae_popup, BOOL fatal){
@@ -51,59 +51,45 @@ void error_popup(NSString *messgae_popup, BOOL fatal){
     }
 }
 
+
+static void RabbitHook(Class cls, SEL selName, IMP replaced, void** orig) {
+    // IGNORE ALL ORIG POINTER
+    Method origMethod = class_getInstanceMethod(cls, selName);
+    *orig = method_setImplementation(origMethod, replaced);
+    printf("(´･ω･`) magic from orig: %p to our %p at class: %s\n", orig, replaced, [NSStringFromClass(cls) UTF8String]);
+}
+
+@class TGAlertController;
+
+static void* (*original_TGAlertController_initAlertWithTitle)(TGAlertController *self, SEL _cmd, NSString* title, NSString* text, NSString* cancelBtn, NSString* otherBtn, id block);
+static void* replaced_TGAlertController_initAlertWithTitle(TGAlertController *self, SEL _cmd, NSString* title, NSString* text, NSString* cancelBtn, NSString* otherBtn, id block) {
+    if ([text containsString:@"Main binary was modified. Please reinstall Filza"]) {
+        NSLog(@"Nope, not runing away");
+        return original_TGAlertController_initAlertWithTitle(self, _cmd, title, @"已经绕过DRM检测，请忽略此弹窗", cancelBtn, otherBtn, NULL);
+    }
+    return original_TGAlertController_initAlertWithTitle(self, _cmd, title, text, cancelBtn, otherBtn, block);
+}
+
+@end
+
+static void cancelExitAlert(void) {
+    RabbitHook(objc_getClass("TGAlertController"), @selector(initAlertWithTitle:text:cancelButton:otherButtons:completion:), (IMP)&replaced_TGAlertController_initAlertWithTitle, (void**)&original_TGAlertController_initAlertWithTitle);
+}
+
 int start() {
     //Start exploitation to gain tfp0.
     Log(log_info, "==Plank Filza==");
-    if(SYSTEM_VERSION_LESS_THAN(@"14.0") || SYSTEM_VERSION_GREATER_THAN(@"14.3")){
+    if(SYSTEM_VERSION_LESS_THAN(@"13.0") || SYSTEM_VERSION_GREATER_THAN(@"14.3")){
         Log(log_error, "Incorrect version");
         error_popup(@"Unsupported iOS version", true);
     } else {
         if(SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(@"14.3")){
             jailbreak(nil);
-            
         }
     }
     return 0;
 }
 
-NSString *notice_json;
-NSString *message;
-BOOL can_show_msg = true;
-BOOL error = false;
-+ (void)showAlert
-{
-    NSURLSession *aSession = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration]];
-    [[aSession dataTaskWithURL:[NSURL URLWithString:@"https://brandonplank.org/json/PlankFilza.json"] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        if(error != nil){
-            Log(log_error, "we got an error...\n");
-        }
-        if (((NSHTTPURLResponse *)response).statusCode == 200) {
-            if (data) {
-                NSString *contentOfURL = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-                //NSLog(@"%@", contentOfURL);
-                notice_json = contentOfURL;
-            } else {
-                Log(log_error, "nil\n");
-            }
-        } else {
-           Log(log_error, "200\n");
-        }
-    }] resume];
-    message = @"Plank Filza by Brandon Plank(@_bplank)\n\nCoryright 2021 Brandon Plank";
-    if(can_show_msg){
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            UIAlertController * alertController = [UIAlertController alertControllerWithTitle:@"Notice" message:message preferredStyle:UIAlertControllerStyleAlert];
-            [alertController addAction:[UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleCancel handler:NULL]];
-            UIViewController * controller = [UIApplication sharedApplication].keyWindow.rootViewController;
-            while (controller.presentedViewController) {
-                controller = controller.presentedViewController;
-            }
-            [controller presentViewController:alertController animated:YES completion:NULL];
-        });
-    } else {
-        Log(log_info, "Not showing msg.\n");
-    }
-}
 __attribute__((constructor))
 static void initializer(void) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
@@ -111,16 +97,24 @@ static void initializer(void) {
         while (main.presentedViewController != NULL && ![main.presentedViewController isKindOfClass: [UIAlertController class]]) {
             main = main.presentedViewController;
         }
-        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Exploiting"
-                                                                       message:@"This will take some time..." preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"漏洞利用中"
+                                                                       message:@"请勿离开当前页面并耐心等待两分钟..." preferredStyle:UIAlertControllerStyleAlert];
         [main presentViewController:alert animated:YES completion:^{
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                 start();
+                free(redeem_racers);
+                cancelExitAlert();
                 [alert dismissViewControllerAnimated:YES completion:^{ }];
+                UIAlertController* alert2 = [UIAlertController alertControllerWithTitle:@"注意"
+                                                                               message:@"PlankFilza由 Brandon Plank、Lakr 和 ModernPwner共同完成。 在适用法律的范围内，我们对您的设备发生的任何不良情况不承担任何责任。" preferredStyle:UIAlertControllerStyleAlert];
+                [alert2 addAction:[UIAlertAction actionWithTitle:@"好" style:UIAlertActionStyleCancel handler:^(UIAlertAction * action) {
+                    [alert2 dismissViewControllerAnimated:YES completion:^{ }];
+                }]];
+                [main presentViewController:alert2 animated:YES completion:nil];
             });
         }];
     });
     
 }
 
-@end
+
